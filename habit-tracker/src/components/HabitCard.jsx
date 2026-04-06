a9fb652ff09a5e955bab356faddfbe2231ca505e
Kritika Kapoor <kritikakapoor131@gmail.com>
2026-04-07 00:28:56 +0530

    Styling improved, Habit Card Array

--- habit-tracker/src/components/HabitCard.jsx
+++ habit-tracker/src/components/HabitCard.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-const HabitCard = () => {
+const HabitCard = ({name, description}) => {
   const [done, setDone] = useState(false)
   return (
     <div className="border-2 border-black rounded-xl p-4 m-5 w-64 bg-white">
-      <h2 className={`text-lg font-semibold mb-2 ${done ? "line-through text-gray-400" : ""}`}>Habit Name</h2>
-      <p className={`text-sm mb-4 ${done ? "text-gray-400" : "text-gray-600"}`}>Description of the habit</p>
+      <h2 className={`text-lg font-semibold mb-2 ${done ? "line-through text-gray-400" : ""}`}>{name}</h2>
+      <p className={`text-sm mb-4 ${done ? "text-gray-400" : "text-gray-600"}`}>{description}</p>
       <div className="flex justify-between items-center">
         <button onClick={() => setDone(!done)} className={`px-3 py-1 rounded-md text-white ${ done ? "bg-green-500" : "bg-gray-500"}`}>{done ? "Done" : "Mark Done"}</button>
         <button className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600">Delete</button>
--- habit-tracker/src/pages/Home.jsx
+++ habit-tracker/src/pages/Home.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import HabitCard from '../components/HabitCard'
 import AddHabitForm from '../components/AddHabitForm'
 
+const habits = [
+  { id: 1, name: "Drink Water", description: "Drink 2l water daily" },
+  { id: 2, name: "Exercise", description: "30 mins workout" },
+  { id: 3, name: "Read", description: "Read 20 pages" },
+]
+
 const Home = () => {
   return (
-    <div>
-      <HabitCard/>
+    <>
       <AddHabitForm/>
-    </div>
+      <div className="flex flex-wrap">
+        {habits.map(habit => (
+          <HabitCard key={habit.id} name={habit.name} description={habit.description} />
+        ))}
+      </div>
+    </>
   )
 }
+
 
 export default Home
