119d6cab2461c0fbf2a9b88023b9cf0dbce8e3b6
Kritika Kapoor <kritikakapoor131@gmail.com>
2026-04-07 00:16:13 +0530

    Add Habit Form

--- habit-tracker/src/components/AddHabitForm.jsx
+++ habit-tracker/src/components/AddHabitForm.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Plus } from 'lucide-react'
 
 const AddHabitForm = () => {
-  return (
-    <div className='fixed top-27 right-4'>
-      <button className="flex items-center gap-1 bg-green-500 text-white px-3 py-1 rounded-md hover:bg-green-600">
-        <Plus size={16} /> Add Habit
-      </button>
-    </div>
-  )
+    const [habit, setHabit] = useState('')
+    const [isOpen, setIsOpen] = useState(false)
+
+    const formHandler = (e) => {
+        e.preventDefault()
+        console.log('New Habit Added is', habit)
+        setHabit('')
+        setIsOpen(false)
+    }
+
+    return (
+        <div className='fixed top-27 right-4'>
+            <button onClick={() => setIsOpen(!isOpen)}
+                className="flex items-center gap-1 bg-green-500 text-white px-3 py-1 rounded-md hover:bg-green-600">
+                <Plus size={16} /> Add Habit
+            </button>
+
+            {isOpen && (
+                <div className="mt-2 bg-white shadow-md rounded-md p-3">
+                    <form onSubmit={formHandler}>
+                        <input type="text" value={habit} placeholder="Enter habit name" onChange={(e) => setHabit(e.target.value)} className="border px-2 py-1 rounded-md w-full mb-2"/>
+                        <button className="bg-green-500 text-white px-3 py-1 rounded-md w-full hover:bg-green-600">Submit</button>
+                    </form>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default AddHabitForm
--- habit-tracker/src/components/Navbar.jsx
+++ habit-tracker/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='bg-blue-500 py-5 gap-7 justify-center flex flex-wrap text-2xl font-semibold'>
-        <Link className='' to='/'>Home</Link>
-        <Link className='' to='/stats'>Stats</Link>
+    <div className='bg-emerald-600 py-5 gap-7 justify-center flex flex-wrap text-2xl font-semibold'>
+        <Link className='text-slate-800' to='/'>Home</Link>
+        <Link className='text-slate-800' to='/stats'>Stats</Link>
     </div>
   )
 }
